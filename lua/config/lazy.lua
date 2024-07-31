d4df52092b9e73084de213516e344e1d2cc09457
xHugo21 <hugo2121h@gmail.com>
2024-08-01 00:24:43 +0200

    feat: update dashboard logo

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,10 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
   },
