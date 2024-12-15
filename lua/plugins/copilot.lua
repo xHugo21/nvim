d38962d4a550398af99a6bd7f3ab7f9c638617ba
xHugo21 <hugo2121h@gmail.com>
2024-12-15 21:53:17 +0100

    feat: tab accepts suggestion instead of enter

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,6 +6,7 @@ return {
       require("copilot").setup({
         suggestion = {
           enabled = true,
+          auto_trigger = true,
           keymap = {
             accept = "<Tab>",
             next = "<C-j>",
@@ -13,6 +14,28 @@ return {
           },
         },
         panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        mapping = {
+          ["<Tab>"] = function(fallback)
+            local copilot_suggestion = require("copilot.suggestion")
+            if copilot_suggestion.is_visible() then
+              copilot_suggestion.accept()
+            elseif cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end,
+        },
       })
     end,
   },
