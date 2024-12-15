3a3a2a2546db1b71e70bc02515958f63f5d4f0b6
xHugo21 <hugo2121h@gmail.com>
2024-12-15 22:20:52 +0100

    refactor: move extras to external file

---
+++ lua/plugins/extras.lua
@@ -0,0 +1,6 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.ui.mini-animate" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+}
