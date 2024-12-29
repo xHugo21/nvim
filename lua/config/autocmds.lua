f7550fb4218eb563d79b3af12d8ddffeafa7be5c
xHugo21 <hugo2121h@gmail.com>
2024-12-29 20:43:58 +0100

    feat: add autocmd to close neo tree when no other buffers are opened

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,3 +9,21 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end,
   desc = "Disable New Line Comment",
 })
+
+-- Close Neo-tree when no more file buffers are open
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  group = vim.api.nvim_create_augroup("AutoCloseNeoTree", { clear = true }),
+  callback = function()
+    local non_floating_windows = vim.tbl_filter(function(win)
+      return vim.api.nvim_win_get_config(win).relative == ""
+    end, vim.api.nvim_tabpage_list_wins(0))
+
+    local non_neo_tree_windows = vim.tbl_filter(function(win)
+      return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "neo-tree"
+    end, non_floating_windows)
+
+    if #non_neo_tree_windows == 0 then
+      vim.cmd("q")
+    end
+  end,
+})
