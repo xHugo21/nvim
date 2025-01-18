a577210c5f6278b9b592380a71ec51d3e7be2ebc
xHugo21 <hugo2121h@gmail.com>
2025-01-18 21:27:57 +0100

    refactor: rename copilot.lua to avante.lua

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -63,39 +63,4 @@ return {
       "zbirenbaum/copilot.lua", -- for providers='copilot'
     },
   },
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   event = "VeryLazy",
-  --   config = function()
-  --     require("copilot").setup({
-  --       suggestion = {
-  --         enabled = true,
-  --         auto_trigger = true,
-  --       },
-  --       panel = { enabled = false },
-  --     })
-  --   end,
-  -- },
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   enabled = true,
-  --   dependencies = { "zbirenbaum/copilot.lua" },
-  --   config = function()
-  --     local cmp = require("cmp")
-  --     cmp.setup({
-  --       mapping = {
-  --         ["<Tab>"] = function(fallback)
-  --           local copilot_suggestion = require("copilot.suggestion")
-  --           if copilot_suggestion.is_visible() then
-  --             copilot_suggestion.accept()
-  --           elseif cmp.visible() then
-  --             cmp.select_next_item()
-  --           else
-  --             fallback()
-  --           end
-  --         end,
-  --       },
-  --     })
-  --   end,
-  -- },
 }
