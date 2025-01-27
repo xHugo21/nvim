5123d6fe6a056bfd8a2e3823435df9e55c814742
xHugo21 <hugo2121h@gmail.com>
2025-01-27 21:40:46 +0100

    feat: add monoglow theme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -25,4 +25,17 @@ return {
   --   "rose-pine/neovim",
   --   priority = 1000,
   -- },
+  {
+    "wnkz/monoglow.nvim",
+    priority = 1000,
+    -- config = function()
+    --   require("monoglow").setup({
+    --     -- Change the "glow" color
+    --     on_colors = function(colors)
+    --       colors.glow = "#fd1b7c" -- Custom glow color
+    --     end,
+    --   })
+    --   vim.cmd("colorscheme monoglow") -- Apply the colorscheme
+    -- end,
+  },
 }
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -2,7 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vesper",
+      colorscheme = "monoglow",
     },
   },
 }
