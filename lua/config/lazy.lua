19068b965e7167649ec113c499fb9989597ff009
xHugo21 <hugo2121h@gmail.com>
2025-03-09 23:16:10 +0100

    feat: disable lazy extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,15 +20,11 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
     -- LazyExtras
-    -- Languages
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- ESLint / Prettier
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    -- Animations
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.linting.eslint" },
+    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- Git diffs (disables gitsigns automatically)
     -- { import = "lazyvim.plugins.extras.editor.mini-diff" },
 
     -- Plugins under plugins folder
