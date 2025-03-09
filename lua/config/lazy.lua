354dec3f781983275aad71b1bc4782ef83fb504e
xHugo21 <hugo2121h@gmail.com>
2025-03-09 22:54:18 +0100

    fix: solve import order by moving extras to lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,20 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
+
+    -- LazyExtras
+    -- Languages
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- ESLint / Prettier
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- Animations
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- Git diffs (disables gitsigns automatically)
+    -- { import = "lazyvim.plugins.extras.editor.mini-diff" },
+
+    -- Plugins under plugins folder
     { import = "plugins" },
   },
   defaults = {
