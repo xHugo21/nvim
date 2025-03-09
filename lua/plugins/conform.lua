92751817650a010497c314b6eb524d7af614b7df
xHugo21 <hugo2121h@gmail.com>
2025-03-09 23:26:05 +0100

    feat: add possible formatters to conform.nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,7 +2,14 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      lua = { "stylua" },
       c = { "clang-format" },
+      json = { "prettier" },
+      -- javascript = { "prettier" },
+      -- typescript = { "prettier" },
+      -- python = { "black" },
+      -- html = { "prettier" },
+      -- css = { "prettier" },
     },
   },
 }
