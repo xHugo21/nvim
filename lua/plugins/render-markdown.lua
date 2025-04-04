beea04844d41ec398528bfcd8b82bb495e6bbf24
xHugo21 <hugo2121h@gmail.com>
2025-04-04 18:06:59 +0200

    feat: update render-markdown to show heading icons overwriten by lazyvim

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+  "MeanderingProgrammer/markdown.nvim",
+  name = "render-markdown",
+  ft = { "markdown", "codecompanion" },
+  opts = {
+    heading = {
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+    },
+  },
+}
