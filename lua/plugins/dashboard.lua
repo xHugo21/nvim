43648d334127775daaabd0a79cf44da94abf8ccd
Hugo Garcia <hugo2121h@gmail.com>
2025-04-07 16:36:15 +0200

    feat: update dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -31,10 +31,48 @@ return {
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+        {
+          icon = " ",
+          desc = "Browse Repository",
+          key = "b",
+          action = function()
+            Snacks.gitbrowse()
+          end,
+        },
           { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
+      },
+      sections = {
+        { section = "header", indent = 60 },
+        { section = "keys", gap = 1, padding = 2 },
+        { indent = -60, pane = 2, padding = 19 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1, pane = 2 },
+        {
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+          pane = 2,
+        },
+        { section = "startup", indent = 60, padding = 1 },
+        function()
+          local version = vim.version()
+          return {
+            align = "center",
+            text = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch,
+            padding = 1,
+            indent = 60,
+          }
+        end,
       },
     },
   },
