4dd22fe58b204cdc92e667c09ae225520f0730ed
Hugo Garcia <hugo2121h@gmail.com>
2025-04-18 10:49:56 +0200

    fix: use make for ARM. Prepend with Esc to avoid no code selected error codesnap

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -1,8 +1,8 @@
 return {
   "mistricky/codesnap.nvim",
-  build = "make build_generator",
+  build = "make",
   keys = {
-    { "<leader>cs", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+    { "<leader>cs", "<Esc><cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
     { "<leader>cd", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
   },
   opts = {
