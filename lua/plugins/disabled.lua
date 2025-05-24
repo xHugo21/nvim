ca8c7a3eea0e48603201244bed9a9f106f9730ce
xHugo21 <hugo2121h@gmail.com>
2025-05-24 08:43:19 +0200

    feat: disable nvim-lint

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "mfussenegger/nvim-lint", enabled = false },
 }
