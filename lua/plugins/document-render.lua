6809dcb707eb9c9ef839ebbd2053f7fcce313e01
xHugo21 <hugo2121h@gmail.com>
2025-07-26 15:01:31 +0200

    feat: add commands to preview and compile typst

--- lua/plugins/document-render.lua
+++ lua/plugins/document-render.lua
@@ -12,5 +12,9 @@ return {
     ft = "typst",
     version = "1.*",
     opts = {},
+    keys = {
+      { "<leader>tp", "<cmd>TypstPreview<CR>", mode = { "n" }, desc = "Typst Preview" },
+      { "<leader>tc", "<cmd>!typst compile %<CR>", mode = { "n" }, desc = "Typst Compile" },
+    },
   },
 }
