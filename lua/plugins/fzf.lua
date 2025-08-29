8066c31825bb7e115a31fffb239eab87763c1ed5
xHugo21 <hugo2121h@gmail.com>
2025-08-29 14:21:14 +0200

    feat: delete keybinding override as <leader>sb is still useful

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,19 +1,5 @@
 return {
   "ibhagwan/fzf-lua",
-  keys = function(_, keys)
-    keys[#keys + 1] = { "<leader>sb", false }
-
-    keys[#keys + 1] = {
-      "<leader>sb",
-      function()
-        require("fzf-lua").buffers()
-      end,
-      desc = "Switch Buffer",
-    }
-
-    return keys
-  end,
-
   opts = function(_, opts)
     return vim.tbl_deep_extend("force", opts or {}, {
       winopts = {
