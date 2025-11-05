ba4990b816644156363bf60ac09cb92a0203c27e
xHugo21 <hugo2121h@gmail.com>
2025-11-05 23:46:31 +0100

    feat: add lazygit and scope snacks plugins

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,1 +1,27 @@
-return {}
+return {
+  {
+    'folke/snacks.nvim',
+    event = 'VeryLazy',
+    opts = {
+      lazygit = {
+        enabled = true,
+        win = {
+          style = 'lazygit',
+          border = 'solid',
+          width = 0.9,
+          height = 0.9,
+        },
+      },
+      scope = {}, -- Scope detection, text objects and jumping based on treesitter or indent
+    },
+    keys = {
+      {
+        '<leader>gg',
+        function()
+          require('snacks').lazygit()
+        end,
+        desc = 'Open Lazygit',
+      },
+    },
+  },
+}
