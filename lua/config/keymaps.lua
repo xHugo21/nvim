bfb0d096e9c4158d9f3ccac086a04345cad97582
xHugo21 <hugo2121h@gmail.com>
2025-11-06 14:51:29 +0100

    feat: set diagnostics open as <leader>xx

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -69,20 +69,21 @@ map('n', 'gcO', 'O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add Commen
 map('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Lazy' })
 
 -- diagnostic
-local diagnostic_goto = function(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-  severity = severity and vim.diagnostic.severity[severity] or nil
-  return function()
-    go { severity = severity }
-  end
-end
-map('n', '<leader>cd', vim.diagnostic.open_float, { desc = 'Line Diagnostics' })
-map('n', ']d', diagnostic_goto(true), { desc = 'Next Diagnostic' })
-map('n', '[d', diagnostic_goto(false), { desc = 'Prev Diagnostic' })
-map('n', ']e', diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
-map('n', '[e', diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
-map('n', ']w', diagnostic_goto(true, 'WARN'), { desc = 'Next Warning' })
-map('n', '[w', diagnostic_goto(false, 'WARN'), { desc = 'Prev Warning' })
+-- local diagnostic_goto = function(next, severity)
+--   local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+--   severity = severity and vim.diagnostic.severity[severity] or nil
+--   return function()
+--     go { severity = severity }
+--   end
+-- end
+
+map('n', '<leader>xx', vim.diagnostic.open_float, { desc = 'Line Diagnostics' })
+-- map('n', ']d', diagnostic_goto(true), { desc = 'Next Diagnostic' })
+-- map('n', '[d', diagnostic_goto(false), { desc = 'Prev Diagnostic' })
+-- map('n', ']e', diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
+-- map('n', '[e', diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
+-- map('n', ']w', diagnostic_goto(true, 'WARN'), { desc = 'Next Warning' })
+-- map('n', '[w', diagnostic_goto(false, 'WARN'), { desc = 'Prev Warning' })
 
 -- toggle options
 -- LazyVim.format.snacks_toggle():map("<leader>uf")
