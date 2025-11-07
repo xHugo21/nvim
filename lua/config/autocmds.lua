f0e170bf05b10d59a99bbfb0991ba164ce3873c0
xHugo21 <hugo2121h@gmail.com>
2025-11-07 09:17:44 +0100

    fix: fix typo on autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -64,7 +64,7 @@ vim.api.nvim_create_autocmd('FileType', {
 -- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd('FileType', {
   group = vim.api.nvim_create_augroup('wrap_spell', { clear = true }),
-  pattern = { 'text', 'plaintex', 'typst', 'gitcommit', 'markdown' },
+  pattern = { 'text', 'plaintext', 'typst', 'gitcommit', 'markdown' },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
