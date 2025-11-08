92d02038489b4fb71eb2444987add9dd5894e764
xHugo21 <hugo2121h@gmail.com>
2025-11-08 12:30:39 +0100

    feat: syntax highlighting autocmd for dotenv files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -90,3 +90,12 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.formatoptions:remove { 'c', 'r', 'o' }
   end,
 })
+
+-- syntax highlighting for dotenv files
+vim.api.nvim_create_autocmd('BufRead', {
+  group = vim.api.nvim_create_augroup('dotenv_ft', { clear = true }),
+  pattern = { '.env', '.env.*' },
+  callback = function()
+    vim.bo.filetype = 'dosini'
+  end,
+})
