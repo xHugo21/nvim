3c0be8022181dcc56adb8038418afc9d7220da92
xHugo21 <hugo2121h@gmail.com>
2025-11-09 18:59:48 +0100

    feat: codecompanion responses show model being used

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -7,6 +7,12 @@ return {
           name = 'copilot',
           model = 'gpt-4.1',
         },
+        roles = {
+          llm = function(adapter)
+            return adapter.formatted_name .. ' (' .. adapter.parameters.model .. ')'
+          end,
+          user = 'Me',
+        },
         slash_commands = {
           ['file'] = { -- Maps <C-p> to enter files as context
             keymaps = {
