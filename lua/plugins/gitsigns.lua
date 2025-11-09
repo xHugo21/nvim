c9c1fa5fa29b836be8b8eda3af85339d453e644a
xHugo21 <hugo2121h@gmail.com>
2025-11-09 23:21:42 +0100

    feat: update git blame and git diff commands, update gitsigns icons

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,36 @@
 return {
   'lewis6991/gitsigns.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
+  opts = {
+    signs = {
+      add = { text = '▎' },
+      change = { text = '▎' },
+      delete = { text = '' },
+      topdelete = { text = '' },
+      changedelete = { text = '▎' },
+      untracked = { text = '▎' },
+    },
+    signs_staged = {
+      add = { text = '▎' },
+      change = { text = '▎' },
+      delete = { text = '' },
+      topdelete = { text = '' },
+      changedelete = { text = '▎' },
+    },
+    on_attach = function(buffer)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
+      end
+
+      -- Git Blame line
+      map('n', '<leader>gb', function()
+        gs.blame_line { full = true }
+      end, 'Blame Line')
+
+      -- Git Diff buffer
+      map('n', '<leader>gd', gs.diffthis, 'Diff This')
+    end,
+  },
 }
