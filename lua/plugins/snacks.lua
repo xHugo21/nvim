24316071a63083f7414943a155f80c4c9e5377fc
xHugo21 <hugo2121h@gmail.com>
2025-11-12 12:41:08 +0100

    feat: customize noice.nvim

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -79,7 +79,21 @@ return {
   { -- Improved UI for command line and messages. Adds LSP status notifications
     'folke/noice.nvim',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+      lsp = { -- Disable LSP signature popup override
+        hover = {
+          enabled = false,
+        },
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = '10%',
+            col = '50%',
+          },
+        },
+      },
+    },
     dependencies = {
       'MunifTanjim/nui.nvim',
     },
