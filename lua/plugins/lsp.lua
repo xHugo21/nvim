a073634a5cfb5e337d7d1c074d8696fa3c312934
xHugo21 <hugo2121h@gmail.com>
2025-12-23 14:25:41 +0100

    feat: lazy load lsp on bufreadpre, bufnewfile

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,7 @@
 return {
   {
     'neovim/nvim-lspconfig',
+    event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       { 'mason-org/mason.nvim', opts = {} },
       'mason-org/mason-lspconfig.nvim',
