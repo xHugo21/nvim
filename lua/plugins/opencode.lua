3e829adc94d23c7a5646ee52caae4c50bb725b82
xHugo21 <hugo2121h@gmail.com>
2026-01-01 18:44:35 +0100

    feat: migrate from codecompanion to opencode

---
+++ lua/plugins/opencode.lua
@@ -0,0 +1,24 @@
+return {
+  'NickvanDyke/opencode.nvim',
+  config = function()
+    ---@type opencode.Opts
+    vim.g.opencode_opts = {
+      provider = {
+        enabled = 'tmux',
+        tmux = {},
+      },
+    }
+
+    vim.o.autoread = true
+
+    vim.keymap.set({ 'n', 't' }, '<leader>aa', function()
+      require('opencode').toggle()
+    end, { desc = 'Toggle opencode' })
+    vim.keymap.set({ 'n', 'x' }, '<leader>ae', function()
+      require('opencode').ask('@this: ', { submit = true })
+    end, { desc = 'Ask opencode' })
+    vim.keymap.set({ 'n', 'x' }, '<leader>ax', function()
+      require('opencode').select()
+    end, { desc = 'Execute opencode action…' })
+  end,
+}
