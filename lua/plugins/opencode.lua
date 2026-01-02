83bb79f6616f6cbd49282e7339c9a69e0d56befb
xHugo21 <hugo2121h@gmail.com>
2026-01-02 08:40:08 +0100

    feat: replace provider on opencode

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -1,15 +1,29 @@
+local tmux_provider = { -- Checks for opencode on tmux window instead of a split
+  toggle = function()
+    local handle = io.popen "tmux list-windows -F '#{window_name}'"
+    if not handle then
+      vim.notify('Failed to run tmux list-windows.', vim.log.levels.ERROR)
+      return
+    end
+    local windows = handle:read '*a'
+    handle:close()
+
+    local cmd = 'opencode'
+
+    if windows and string.find(windows, 'opencode') then
+      os.execute 'tmux select-window -t opencode'
+    else
+      os.execute("tmux new-window -n opencode '" .. cmd .. "'")
+    end
+  end,
+}
+
 return {
   'NickvanDyke/opencode.nvim',
   config = function()
     ---@type opencode.Opts
     vim.g.opencode_opts = {
-      provider = {
-        enabled = 'tmux',
-        tmux = {
-          options = '-h -p 40',
-        },
-      },
-
+      provider = tmux_provider,
       prompts = {
         commit = {
           prompt = "Generate a Conventional Commit message for the following changes: @diff. Return ONLY the raw commit message (e.g. 'feat: add feature'). Do NOT include the scope/parenthesis part (e.g. do NOT use 'feat(core): add feature'). Do not wrap in markdown or provide explanations.",
