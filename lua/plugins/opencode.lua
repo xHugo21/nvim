b0dbf3c1ef22cca44feb26a331753506e9a4460e
xHugo21 <hugo2121h@gmail.com>
2026-01-06 10:20:45 +0100

    feat: disable render markdown on markdown, leave on opencode window

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -39,9 +39,9 @@ return {
       'MeanderingProgrammer/render-markdown.nvim',
       opts = {
         anti_conceal = { enabled = false },
-        file_types = { 'markdown', 'opencode_output' },
+        file_types = { 'opencode_output' },
       },
-      ft = { 'markdown', 'opencode_output' },
+      ft = { 'opencode_output' },
     },
     'saghen/blink.cmp',
     'ibhagwan/fzf-lua',
