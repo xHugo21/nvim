c8db231ca0fde50f5d6d1f4bd9d75b156f141150
xHugo21 <hugo2121h@gmail.com>
2026-04-26 20:46:07 +0200

    feat: migrate to treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,25 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  opts = {
-    ensure_installed = { 'bash', 'regex', 'diff', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vimdoc' },
-    auto_install = true,
-    highlight = {
-      enable = true,
-    },
-    indent = { enable = true },
-  },
+  branch = "main",
+  main = 'nvim-treesitter',
+  init = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+
+    local ensureInstalled = {
+      'bash', 'regex', 'diff', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vimdoc',
+    }
+    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+    local parsersToInstall = vim.iter(ensureInstalled)
+        :filter(function(parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+    require('nvim-treesitter').install(parsersToInstall)
+  end,
 }
